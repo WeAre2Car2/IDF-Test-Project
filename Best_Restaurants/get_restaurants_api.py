3bed2c97026dfafd31031524cbae8523cfc34532
Nadav <nadavnegbi123@gmail.com>
2024-06-11 22:51:21 +0300

    Made the API work as intended, Added a get_restaurant_api

--- Best_Restaurants/get_restaurants_api.py
+++ Best_Restaurants/get_restaurants_api.py
@@ -4,18 +4,18 @@ import requests
 
 32.474962, 34.976855
 def main():
-  client = serpapi.Client(api_key="f9ae0429c9b651cbee59c3229372fadeefe5c448360b1d791b13b099b9baf303")
-  result = client.search(
-    q="Restaurant",
-    engine="google_local",
-    num="2",
-    type="search",
-    location="New York, United States"
-
-  )
-  restaurants = result["local_results"]
-  restaurants_json = json.dumps(restaurants, indent = 4)
-  return restaurants_json
+  params = {
+  "engine": "google_maps",
+  "q": "Restaurant",
+  "type": "search",
+  "location": "New York, New York, United States",
+  "num": "2",
+  "api_key": "f9ae0429c9b651cbee59c3229372fadeefe5c448360b1d791b13b099b9baf303"
+}
+  search = serpapi.search(params)
+  results = search.as_dict()
+  json_string = json.dumps(results, indent=4)  
+  return json_string 
 
 if __name__ == "__main__":
   main()
--- Best_Restaurants/restaurants.py
+++ Best_Restaurants/restaurants.py
@@ -1,4 +1,5 @@
 import json
+import get_restaurants_api as json_get
 
 # Creating a restaurant class, each resturant object will hold 1 resturant info
 class Restaurant():
@@ -9,16 +10,14 @@ class Restaurant():
     self.resturant_list = []
   
   def sort_JSON(): #sorts the JSON so that the best rating will be on top and saves to a new file!
-    with open('F:\Projects\IDF_Test_Project\Best_Restaurants\search_example.json', 'r') as openfile:
-      # Reading from json file
-      data = json.load(openfile)
+    data = json_get.main() # Loads to string
+    data_json = json.loads(data) # Converts to json
     # Sort the local_results based on the rating in descending order
-    sorted_results = sorted(data['local_results'], key=lambda x: x['rating'], reverse=True)
+    sorted_results = sorted(data_json['local_results'], key=lambda x: x['rating'], reverse=True)
     # Update the original data with the sorted results
-    data['local_results'] = sorted_results
-    # Print the sorted data
-    with open('Best_Restaurants/sorted_data.json', 'w') as outfile:
-      json.dump(data, outfile, indent=2)
+    data_json['local_results'] = sorted_results
+    # Returns the sorted data
+    return data_json
 
   # Gets and Sets the values of the object
   def get_restaurant_name(self):
@@ -37,10 +36,7 @@ class Restaurant():
     self.resturant_list.append(restaurant)
     
   def get_titles():
-    with open('F:\Projects\IDF_Test_Project\Best_Restaurants\sorted_data.json', 'r') as openfile:
-  
-      # Reading from json file
-      data = json.load(openfile)
+    data = Restaurant.sort_JSON()
 
     title_list = []
     for result in data["local_results"]:
@@ -49,9 +45,7 @@ class Restaurant():
   
   # Retrieves the ratings from an example JSON file
   def get_ratings():
-    with open('F:\Projects\IDF_Test_Project\Best_Restaurants\sorted_data.json', 'r') as openfile:
-      # Reading from json file
-      data = json.load(openfile)
+    data = Restaurant.sort_JSON()
 
     rating_list = []
     for result in data["local_results"]:
@@ -60,10 +54,7 @@ class Restaurant():
   
   # Retrieves the open status from given value, no open status in the JSON example file
   def get_open_status():
-    with open('F:\Projects\IDF_Test_Project\Best_Restaurants\sorted_data.json', 'r') as openfile:
-  
-      # Reading from json file
-      data = json.load(openfile)
+    data = Restaurant.sort_JSON()
 
     is_open_list = []
     for result in data["local_results"]:
@@ -79,8 +70,13 @@ class Restaurant():
     open_status = Restaurant.get_open_status()
     # Creating a list of restaurant objects
     restaurant_list = []
-    for i in range(num_of_restaurants):
-      restaurant_list.append(Restaurant(title_list[i], rating_list[i], open_status[i]))
+    try:
+      for i in range(num_of_restaurants):
+        restaurant_list.append(Restaurant(title_list[i], rating_list[i], open_status[i]))
+    
+    except IndexError:
+      print("Out Of Bounds")
+    
     return restaurant_list
 
 def main():
