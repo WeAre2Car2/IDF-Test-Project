f7dd956809fc1e8d5578903cdae1d5dc0dfe1bc3
Nadav <nadavnegbi123@gmail.com>
2024-06-11 22:56:53 +0300

    Cleaned up the file directory and the get_restaurant_api

--- Best_Restaurants/get_restaurants_api.py
+++ Best_Restaurants/get_restaurants_api.py
@@ -1,9 +1,7 @@
 import serpapi
 import json
-import requests
 
-32.474962, 34.976855
-def main():
+def main(): # Parameters to get restaurants in Ny since hebrew doesnt work
   params = {
   "engine": "google_maps",
   "q": "Restaurant",
@@ -12,10 +10,9 @@ def main():
   "num": "2",
   "api_key": "f9ae0429c9b651cbee59c3229372fadeefe5c448360b1d791b13b099b9baf303"
 }
-  search = serpapi.search(params)
-  results = search.as_dict()
-  json_string = json.dumps(results, indent=4)  
-  return json_string 
-
+  search = serpapi.search(params) # Searches
+  results = search.as_dict() # converts to dict
+  json_string = json.dumps(results, indent=4)  # to str
+  return json_string #returns the str
 if __name__ == "__main__":
   main()
