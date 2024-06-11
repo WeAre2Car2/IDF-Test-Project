c9c8bbfb0da87523c5ba9f8eb6de61e1c67476ff
Nadav <nadavnegbi123@gmail.com>
2024-06-11 19:57:11 +0300

    started working on the api

--- Best_Restaurants/get_restaurants_api.py
+++ Best_Restaurants/get_restaurants_api.py
@@ -1,11 +1,21 @@
-#import serpapi
-# params = {
-#   "engine": "google_local",
-#   "q": "מסעדות",
-#   "location": "Pardes Hana Karkur, Israel",
-#   "api_key": ""
-# }
+import serpapi
+import json
+import requests
 
-# search = serpapi.GoogleSearch(params)
-# results = search.get_dict()
-# local_results = results["local_results"]
+32.474962, 34.976855
+def main():
+  client = serpapi.Client(api_key="f9ae0429c9b651cbee59c3229372fadeefe5c448360b1d791b13b099b9baf303")
+  result = client.search(
+    q="Restaurant",
+    engine="google_local",
+    num="2",
+    type="search",
+    location="New York, United States"
+
+  )
+  restaurants = result["local_results"]
+  restaurants_json = json.dumps(restaurants, indent = 4)
+  return restaurants_json
+
+if __name__ == "__main__":
+  main()
