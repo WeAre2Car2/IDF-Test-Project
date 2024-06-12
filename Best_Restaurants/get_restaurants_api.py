7d543c8998dd174c25f00efffadad15da2747570
Nadav <nadavnegbi123@gmail.com>
2024-06-12 12:19:49 +0300

    Added docker thingie

--- Best_Restaurants/get_restaurants_api.py
+++ Best_Restaurants/get_restaurants_api.py
@@ -5,7 +5,7 @@ def main(): # Parameters to get restaurants in Ny since hebrew doesnt work
   params = {
   "engine": "google_maps",
   "q": "Restaurant",
-  "ll": "@32.4784171,34.9826177,15z",
+  "ll": "@32.4742437,34.993965,15z",
   "type": "search",
   "api_key": "f9ae0429c9b651cbee59c3229372fadeefe5c448360b1d791b13b099b9baf303"
 }
